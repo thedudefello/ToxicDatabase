baa21043fc3aed434bc69275851d7cc4100b8927
Felloyuuka <123282337+FelloBoiYuuka@users.noreply.github.com>
2025-01-22 19:50:43 -0700

    By @MalwareStudio

---
+++ Source codes/RSOD/RSOD/Form1.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RSOD
+{
+    public partial class RSOD : Form
+    {
+        public RSOD()
+        {
+            InitializeComponent();
+        }
+
+        private void RSOD_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+        }
+
+        private void RSOD_Load(object sender, EventArgs e)
+        {
+           
+            
+        }
+    }
+}
