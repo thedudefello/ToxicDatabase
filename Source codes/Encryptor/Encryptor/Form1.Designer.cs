166cb80c4bfdb21074c285076d924ec72e8b7426
Felloyuuka <123282337+FelloBoiYuuka@users.noreply.github.com>
2025-01-22 19:51:40 -0700

    By @MalwareStudio

---
+++ Source codes/Encryptor/Encryptor/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿
+namespace Encryptor
+{
+    partial class Encryptor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Encryptor));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 29);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(314, 95);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(12, 143);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(314, 95);
+            this.listBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Files";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Folders";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 257);
+            this.textBox1.MaxLength = 8;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 241);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Password";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(332, 101);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(332, 72);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(81, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Add";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(332, 186);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(81, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Add";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(332, 215);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(81, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Clear";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 314);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(137, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Encrypt";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(155, 314);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(124, 23);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Decrypt";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(285, 314);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(124, 23);
+            this.button7.TabIndex = 12;
+            this.button7.Text = "Clear All";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Encryptor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 349);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Encryptor";
+            this.Text = "Encryptor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ Source codes/Encryptor/Encryptor/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Encryptor
+{
+    public partial class Encryptor : Form
+    {
+        public Encryptor()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Add files
+            OpenFileDialog filepath = new OpenFileDialog();
+            filepath.Title = "Select File";
+            filepath.InitialDirectory = @"C:\";
+            filepath.Filter = "All files (*.*)|*.*";
+            filepath.Multiselect = true;
+            filepath.FilterIndex = 1;
+            filepath.ShowDialog();
+            foreach(String file in filepath.FileNames)
+            {
+                listBox1.Items.Add(file); //add file path to the listbox
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Add folders
+            FolderBrowserDialog folderpath = new FolderBrowserDialog();
+            folderpath.ShowDialog();
+            listBox2.Items.Add(folderpath.SelectedPath);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //clear all values in listbox1
+            listBox1.Items.Clear();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //clear all values in listbox2
+            listBox2.Items.Clear();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            //Clear values from listbox1, 2 and password text line
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            textBox1.Text = "";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //This function will encrypt selected files
+            //Password must have 8 characters!
+            if(textBox1.Text.Length <8)
+            {
+                MessageBox.Show("Password must have 8 characters!", "Invalid Password", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            //This is for selected files
+            if(listBox1.Items.Count > 0)
+            {
+                for(int num = 0; num < listBox1.Items.Count; num++)
+                {
+                    string e_file = "" + listBox1.Items[num];
+                    if(!e_file.Trim().EndsWith(".!LOCKED") && File.Exists(e_file))
+                    {
+                        EncryptFile("" + listBox1.Items[num], "" + listBox1.Items[num] + ".!LOCKED", textBox1.Text);
+                        File.Delete("" + listBox1.Items[num]);
+                    }
+                }
+            }
+            //This is for selected folders
+            if (listBox2.Items.Count > 0)
+            {
+                for (int num = 0; num < listBox2.Items.Count; num++)
+                {
+                    string d_file = "" + listBox2.Items[num];
+                    string[] get_files = Directory.GetFiles(d_file);
+                    foreach(string name_file in get_files)
+                    {
+                        if (!name_file.Trim().EndsWith(".!LOCKED") && File.Exists(name_file))
+                        {
+                            EncryptFile(name_file, name_file + ".!LOCKED", textBox1.Text);
+                            File.Delete(name_file);
+                        }
+                    }
+                }
+            }
+        }
+
+        char[] mychar = { '!', '.', 'L', 'O', 'C', 'K', 'E', 'D' };
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //This function will decrypt selected files
+            //Password must have 8 characters!
+            //Password must be correct!
+            if (textBox1.Text.Length < 8)
+            {
+                MessageBox.Show("Password must have 8 characters!", "Invalid Password", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            //This is for selected files
+            if (listBox1.Items.Count > 0)
+            {
+                for (int num = 0; num < listBox1.Items.Count; num++)
+                {
+                    string e_file = "" + listBox1.Items[num];
+                    if (e_file.Trim().EndsWith(".!LOCKED") && File.Exists(e_file))
+                    {
+                        DecryptFile(e_file, e_file.TrimEnd(mychar), textBox1.Text);
+                        File.Delete(e_file);
+                    }
+                }
+            }
+            //This is for selected folders
+            if (listBox2.Items.Count > 0)
+            {
+                for (int num = 0; num < listBox2.Items.Count; num++)
+                {
+                    string d_file = "" + listBox2.Items[num];
+                    string[] get_files = Directory.GetFiles(d_file);
+                    foreach (string name_file in get_files)
+                    {
+                        if (name_file.Trim().EndsWith(".!LOCKED") && File.Exists(name_file))
+                        {
+                            DecryptFile(name_file, name_file.TrimEnd(mychar), textBox1.Text);
+                            File.Delete(name_file);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void DecryptFile(string inputFile, string outputFile, string password)
+        {
+            try
+            {
+                UnicodeEncoding UE = new UnicodeEncoding();
+                byte[] key = UE.GetBytes(password);
+
+                FileStream fsCrypt = new FileStream(inputFile, FileMode.Open);
+
+                RijndaelManaged RMCrypto = new RijndaelManaged();
+
+                CryptoStream cs = new CryptoStream(fsCrypt,
+                    RMCrypto.CreateDecryptor(key, key),
+                    CryptoStreamMode.Read);
+
+                FileStream fsOut = new FileStream(outputFile, FileMode.Create);
+
+                int data;
+                while ((data = cs.ReadByte()) != -1)
+                    fsOut.WriteByte((byte)data);
+
+                fsOut.Close();
+                cs.Close();
+                fsCrypt.Close();
+            }
+            catch { }
+        }
+
+        private void EncryptFile(string inputFile, string outputFile, string password)
+        {
+            try
+            {
+                UnicodeEncoding UE = new UnicodeEncoding();
+                byte[] key = UE.GetBytes(password);
+
+                string cryptFile = outputFile;
+                FileStream fsCrypt = new FileStream(cryptFile, FileMode.Create);
+
+                RijndaelManaged RMCrypto = new RijndaelManaged();
+
+                CryptoStream cs = new CryptoStream(fsCrypt,
+                    RMCrypto.CreateEncryptor(key, key),
+                    CryptoStreamMode.Write);
+
+                FileStream fsIn = new FileStream(inputFile, FileMode.Open);
+
+                int data;
+                while ((data = fsIn.ReadByte()) != -1)
+                    cs.WriteByte((byte)data);
+
+
+                fsIn.Close();
+                cs.Close();
+                fsCrypt.Close();
+            }
+            catch { }
+        }
+    }
+}
